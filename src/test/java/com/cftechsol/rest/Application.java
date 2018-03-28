d46788f26500076c008b50014bf99f8714a4c59d
Caio Frota <caiofrota@gmail.com>
2018-03-28 20:16:05 -0300

    Defining test.

--- src/test/java/com/cftechsol/rest/Application.java
+++ src/test/java/com/cftechsol/rest/Application.java
@@ -3,9 +3,11 @@ package com.cftechsol.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@SpringBootTest
 public class Application {
 
 	public static void main(String[] args) {
