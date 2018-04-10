836e8e9d56c20906a766e8656cdf7b0847fdc968
Caio Frota <caiofrota@gmail.com>
2018-04-10 16:30:19 -0300

    Clean code.

--- src/test/java/com/cftechsol/rest/Application.java
+++ src/test/java/com/cftechsol/rest/Application.java
@@ -1,6 +1,5 @@
 package com.cftechsol.rest;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
@@ -8,8 +7,4 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-	
 }
